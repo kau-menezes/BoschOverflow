f40deb312c6377611cc02a8f13f1b4e21ead7b96
AmiltonMn <amilton.moreiran@hotmail.com>
2024-11-12 10:46:17 -0300

    Infrastructure of the project with folders

--- src/main/java/com/example/demo/services/AnswerService.java
+++ src/main/java/com/example/demo/services/AnswerService.java
@@ -1,5 +1,9 @@
 package com.example.demo.services;
 
+import org.springframework.http.ResponseEntity;
+
+import com.example.demo.models.AnswerEntity;
+
 public interface AnswerService {
-    
+    ResponseEntity<AnswerEntity> createAnswer(String answerText);
 }
