580c444f1ab7acb416c2f18e0f508ec832f36361
sasa2754 <sasamor5@gmail.com>
2024-11-18 08:09:20 -0300

    aaaa

--- src/main/java/com/example/demo/config/DependencyConfiguration.java
+++ src/main/java/com/example/demo/config/DependencyConfiguration.java
@@ -2,18 +2,38 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
+import com.example.demo.impl.AnswerImpl;
+import com.example.demo.impl.QuestionImpl;
+import com.example.demo.impl.SpaceImpl;
+import com.example.demo.impl.UserImpl;
+import com.example.demo.services.AnswerService;
+import com.example.demo.services.QuestionService;
+import com.example.demo.services.SpaceService;
 import com.example.demo.services.UserService;
 
 @Configuration
 public class DependencyConfiguration {
     
     @Bean
-    @Scope("singleton")
     public UserService userService() {
-        return null; // retorna nulo só pra não dar erro rsrsrsrs IMPLEMENTAR O SERVIÇO DE USUÁRIO AQUI
-    } 
+        return new UserImpl();
+    }
+
+    @Bean
+    public SpaceService spaceService() {
+        return new SpaceImpl();
+    }
+
+    @Bean
+    public QuestionService questionService() {
+        return new QuestionImpl();
+    }
+
+    @Bean
+    public AnswerService answerService() {
+        return new AnswerImpl();
+    }
 
 
 }
