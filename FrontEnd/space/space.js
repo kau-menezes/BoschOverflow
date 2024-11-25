a156c8251190ac5c900e12486fb86df1eb8494b5
milena579 <milecalegari@gmailcom>
2024-11-25 11:51:39 -0300

    permissions começada

--- FrontEnd/space/space.js
+++ FrontEnd/space/space.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const formSpace = document.getElementById('formSpace');
-
+    const formPermission = document.getElementById('formPermission');
+    
     //add um evento submit ao formulario
     formSpace.addEventListener('submit', async(e) => {
         e.preventDefault();
 
         const spaceValue = document.getElementById('newSpace').value
-
-    
         //FETCH API -ENVIAR OS DADOS VIS REQUEST(POST) PARA O SERVIDOR
         
         var data = sessionStorage.getItem('token');
@@ -40,6 +39,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if( spaceValue != ""){  
             alert("Space criado com sucesso!")
+        }
+
+
+        location.reload("http://127.0.0.1:5500/FrontEnd/space/home.html");
+    })
+
+
+        //add um evento submit ao formulario
+    formPermission.addEventListener('submit', async(e) => {
+        e.preventDefault();
+
+        const emailValue = document.getElementById('email').value
+        const spaceValue = document.getElementById('spaceID').value
+        const permissonValue = document.getElementById('permissionName').value
+
+        //FETCH API -ENVIAR OS DADOS VIS REQUEST(POST) PARA O SERVIDOR
+
+
+        if(emailValue ==  "" || spaceValue == "" || permissonValue ==  ""){
+            window.alert("O campo de nome não pode ser vazio")
+            return;
+        }
+
+        const formData = {
+            email: emailValue,
+            spaceID: spaceValue,
+            newPermission: permissonValue
+        }
+
+        const response = await fetch('http://localhost:8080/permission',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization':  data,
+                'Origin' : "http://127.0.0.1:5500"
+            },
+
+            body: JSON.stringify(formData) //converte o objeto em string json
+        })
+
+        if( spaceValue != ""){  
+            alert("Permissao criado com sucesso!")
         }
 
 
