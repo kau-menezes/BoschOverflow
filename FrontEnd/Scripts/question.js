cdd8c18e19c0430cf654787196335c798258fdcf
WallyDev <wallace07abreu@gmail.com>
2024-11-26 09:30:22 -0300

    feito get e create da question

--- FrontEnd/Scripts/question.js
+++ FrontEnd/Scripts/question.js
@@ -1,34 +1,41 @@
+window.addEventListener("load",searchQuestions(sessionStorage.getItem("spaceId")));
+
+
 var deleteForm = document.getElementById("deleteQuestion")
 var token = sessionStorage.getItem("userToken")
 var addBtn = document.getElementById("btnAdd")
+var lista = document.getElementById("list_questions")
 
-addBtn.addEventListener("click", function() {
+addBtn.addEventListener("click", () => createQuestion(token) )
+
+async function createQuestion(token) {
 
     console.log('fala fi')
 
-    var userId = parseInt(sessionStorage.getItem("userId"))
-    console.log(document.getElementById("newQuestionTitle").value);
-    console.log(document.getElementById('newQuestionText').value);
-    
-    
+    var userId = sessionStorage.getItem("userId")
     data = {
         "questionTitle": document.getElementById("newQuestionTitle").value,
         "questionText": document.getElementById('newQuestionText').value,
-        "spaceId": 7,
+        "spaceId": sessionStorage.getItem("spaceId"),
         "userId": userId
     }
 
-    fetch("http://localhost:8080/question", 
+    const response = await fetch("http://localhost:8080/question", 
         {
+            method: "POST",
             headers: {
                 'Authorization': token,
+                'Content-Type': 'application/json'
             },
-            method: "POST",
             body: JSON.stringify(data)
     })
-    // .then(res => res.json())
-    .then(res => console.log(res))
-})
+    console.log(response);
+    
+    const info = await response.text()
+    console.log(info);
+    
+
+}
 
 deleteForm.addEventListener("submit", function() {
 
@@ -47,3 +54,62 @@ deleteForm.addEventListener("submit", function() {
     .then(res => res.json())
     .then(res => console.log(res))
 })
+
+
+
+async function searchQuestions(spaceId){
+
+    console.log("oi")
+
+    const httpMethod = {
+        method: 'GET',
+        headers: {
+                'Authorization' : sessionStorage.getItem("userToken")
+            }
+        };
+        var response = await fetch("http://localhost:8080/question/" + spaceId, httpMethod );
+        var questions = await response.json()
+        console.log(questions)
+
+        questions.forEach(question => {
+        console.log(question);
+        
+            const a = document.createElement("a")
+
+            const li = document.createElement("li")
+            li.setAttribute("class", "list-group-item d-flex justify-content-between align-items-start")
+
+
+            const div = document.createElement("div")
+            div.setAttribute("class", "ms-2 me-auto")
+
+            const questionTitle = document.createElement("div")
+            questionTitle.setAttribute("class", "fw-bold")
+            questionTitle.textContent = `${question.questionTitle}`;
+
+            const questionText = document.createElement("p")
+            questionText.textContent = `${question.questionText}`;
+
+
+            const id = document.createElement("span")
+            id.setAttribute("class", "badge rounded-pill")
+            id.textContent = `${question.questionId}`;
+
+            div.appendChild(questionText)
+            div.appendChild(questionTitle)
+            li.appendChild(div)
+            li.appendChild(id)
+
+            
+            a.appendChild(li)
+            a.addEventListener("click",(event) => {
+                event.preventDefault()
+                sessionStorage.setItem("questionId",question.questionId)
+                window.location.replace("http://127.0.0.1:5500/answer.html");
+            })
+            
+            lista.appendChild(a)
+            
+        });
+        
+    }
