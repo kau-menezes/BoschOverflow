80e6db1eba8485ebebb18fa8b6013470e6a28958
WallyDev <wallace07abreu@gmail.com>
2024-11-26 11:47:27 -0300

    nao vai dar tempo q nao tinha back, ai criamos mas demorou demais

--- FrontEnd/Scripts/Answers.js
+++ FrontEnd/Scripts/Answers.js
@@ -31,6 +31,55 @@ async function createAnswer(token){
         console.log(info);
         
     
-    
+async function searchAnswers(questionId){
 
+            console.log("oi")
+        
+            const httpMethod = {
+                method: 'GET',
+                headers: {
+                        'Authorization' : sessionStorage.getItem("userToken")
+                    }
+                };
+                var response = await fetch("http://localhost:8080/question/byId/" + questionId, httpMethod );
+                var answers = await response.json()
+                console.log(questions)
+        
+                answers.forEach(answer => {
+                console.log(question);
+                
+        
+                    const li = document.createElement("li")
+                    li.setAttribute("class", "list-group-item d-flex justify-content-between align-items-start")
+        
+        
+                    const div = document.createElement("div")
+                    div.setAttribute("class", "ms-2 me-auto")
+        
+                    const questionTitle = document.createElement("div")
+                    questionTitle.setAttribute("class", "fw-bold")
+                    questionTitle.textContent = `${answer.questionTitle}`;
+        
+                    const questionText = document.createElement("p")
+                    questionText.textContent = `${answer.questionText}`;
+        
+        
+                    const id = document.createElement("span")
+                    id.setAttribute("class", "badge rounded-pill")
+                    id.textContent = `${answer.questionId}`;
+        
+
+                    const lista = document.getElementById("list_answer")
+
+                    div.appendChild(questionText)
+                    div.appendChild(questionTitle)
+                    li.appendChild(div)
+                    li.appendChild(id)
+        
+                    
+                    lista.appendChild(li)
+                    
+                });
+                
+}
 }
