d10368f823453632efe4aa64dbd7b5737c150925
WallyDev <wallace07abreu@gmail.com>
2024-11-27 09:27:43 -0300

    getAnswers feito

--- FrontEnd/Scripts/Answers.js
+++ FrontEnd/Scripts/Answers.js
@@ -1,85 +1,82 @@
-const answer = document.getElementById("answerInput")
-const button = document.getElementById("answerCreate") 
-const questionId = sessionStorage.getItem("questionId")
-const token = sessionStorage.getItem("userToken")
-const userId = sessionStorage.getItem("userId")
+window.addEventListener("load", searchAnswers(sessionStorage.getItem("questionId")))
 
 
-button.addEventListener("click", () => createAnswer(token))
+const answer = document.getElementById("answerInput");
+const button = document.getElementById("answerCreate");
+const questionId = sessionStorage.getItem("questionId");
+const token = sessionStorage.getItem("userToken");
+const userId = sessionStorage.getItem("userId");
 
-async function createAnswer(token){
-        alert(answer.value);
-        
-        data = {
-            "text": answer.value,
-            "questionId": questionId
- 
-        }
-    
-        const response = await fetch("http://localhost:8080/answer", 
-            {
-                method: "POST",
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-        })
-        
-        const info = await response.text()
-        location.reload("http://127.0.0.1:5500/answer.html");
-        console.log(info);
-        
-    
-async function searchAnswers(questionId){
+const questionTitle = document.getElementById("questionTitle");
+const questionText = document.getElementById("questionText");
 
-            console.log("oi")
-        
-            const httpMethod = {
-                method: 'GET',
-                headers: {
-                        'Authorization' : sessionStorage.getItem("userToken")
-                    }
-                };
-                var response = await fetch("http://localhost:8080/question/byId/" + questionId, httpMethod );
-                var answers = await response.json()
-                console.log(questions)
-        
-                answers.forEach(answer => {
-                console.log(question);
-                
-        
-                    const li = document.createElement("li")
-                    li.setAttribute("class", "list-group-item d-flex justify-content-between align-items-start")
-        
-        
-                    const div = document.createElement("div")
-                    div.setAttribute("class", "ms-2 me-auto")
-        
-                    const questionTitle = document.createElement("div")
-                    questionTitle.setAttribute("class", "fw-bold")
-                    questionTitle.textContent = `${answer.questionTitle}`;
-        
-                    const questionText = document.createElement("p")
-                    questionText.textContent = `${answer.questionText}`;
-        
-        
-                    const id = document.createElement("span")
-                    id.setAttribute("class", "badge rounded-pill")
-                    id.textContent = `${answer.questionId}`;
-        
+button.addEventListener("click", () => createAnswer(token));
 
-                    const lista = document.getElementById("list_answer")
+async function createAnswer(token) {
+  alert(answer.value);
 
-                    div.appendChild(questionText)
-                    div.appendChild(questionTitle)
-                    li.appendChild(div)
-                    li.appendChild(id)
-        
-                    
-                    lista.appendChild(li)
-                    
-                });
-                
+  data = {
+    text: answer.value,
+    questionId: questionId,
+  };
+
+  const response = await fetch("http://localhost:8080/answer", {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const info = await response.text();
+  location.reload("http://127.0.0.1:5500/answer.html");
+  console.log(info);
 }
-}
+  async function searchAnswers(questionId) {
+    console.log("oi");
+
+    const httpMethod = {
+      method: "GET",
+      headers: {
+        Authorization: sessionStorage.getItem("userToken"),
+      },
+    };
+    var response = await fetch(
+      "http://localhost:8080/question/byId/" + questionId,
+      httpMethod
+    );
+    var info = await response.json();
+    console.log(info);
+
+    questionTitle.textContent = `${info.questionTitle}`;
+    questionText.textContent = `${info.questionText}`;
+    info.answers.forEach((answer) => {
+      console.log(answer);
+
+      const li = document.createElement("li");
+      li.setAttribute(
+        "class",
+        "list-group-item d-flex justify-content-between align-items-start"
+      );
+
+      const div = document.createElement("div");
+      div.setAttribute("class", "ms-2 me-auto");
+
+      const questionTitle = document.createElement("div");
+      questionTitle.setAttribute("class", "fw-bold");
+      questionTitle.textContent = `${answer.userName}`;
+
+      const questionText = document.createElement("p");
+      questionText.textContent = `${answer.text}`;
+
+      const lista = document.getElementById("list_answer");
+
+      div.appendChild(questionTitle);
+      div.appendChild(questionText);
+      li.appendChild(div);
+
+      lista.appendChild(li);
+    });
+  }
+
